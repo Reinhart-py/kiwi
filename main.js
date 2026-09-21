90f06729962ec6b69025fc96a6608c3133b57e33
Reinhart <196166520+Reinhart-py@users.noreply.github.com>
2026-09-21 11:06:37 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,151 @@
+const { app, BrowserWindow, ipcMain, dialog, shell } = require('electron');
+const path = require('path');
+const { verifyKey, getSavedKey, saveKey, removeKey } = require('./src/license');
+const { getHistory, getActiveCheckpoint, clearActiveCheckpoint, getExportPath } = require('./src/storage');
+const { runGmaps } = require('./src/scraper-gmaps');
+const { runTwoGis } = require('./src/scraper-2gis');
+
+let mainWindow = null;
+let activeTask = null;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1280,
+    height: 820,
+    minWidth: 980,
+    minHeight: 640,
+    backgroundColor: '#0B2416',
+    autoHideMenuBar: true,
+    icon: path.join(__dirname, 'images', 'icon.ico'),
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+      nodeIntegration: false
+    }
+  });
+
+  mainWindow.loadFile(path.join(__dirname, 'renderer', 'index.html'));
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+ipcMain.handle('license:check-saved', async () => {
+  const key = getSavedKey();
+  if (!key) return { passed: false };
+  return await verifyKey(key);
+});
+
+ipcMain.handle('license:verify', async (event, key) => {
+  const res = await verifyKey(key);
+  if (res.passed) {
+    saveKey(key);
+  }
+  return res;
+});
+
+ipcMain.handle('license:logout', async () => {
+  removeKey();
+  return true;
+});
+
+ipcMain.handle('storage:get-history', async () => {
+  return getHistory();
+});
+
+ipcMain.handle('storage:get-active-checkpoint', async () => {
+  return getActiveCheckpoint();
+});
+
+ipcMain.handle('storage:dismiss-checkpoint', async () => {
+  clearActiveCheckpoint();
+  return true;
+});
+
+ipcMain.handle('dialog:open-file', async () => {
+  const res = await dialog.showOpenDialog(mainWindow, {
+    properties: ['openFile'],
+    filters: [
+      { name: 'Spreadsheets and Text', extensions: ['csv', 'xlsx', 'xls', 'txt'] },
+      { name: 'All Files', extensions: ['*'] }
+    ]
+  });
+  if (!res.canceled && res.filePaths.length > 0) {
+    return res.filePaths[0];
+  }
+  return null;
+});
+
+ipcMain.handle('shell:open-link', async (event, url) => {
+  await shell.openExternal(url);
+});
+
+ipcMain.handle('scraper:start-gmaps', async (event, config) => {
+  if (activeTask && activeTask.isRunning) {
+    return { success: false, message: 'Another task is currently running.' };
+  }
+
+  activeTask = { isRunning: true, cancelled: false };
+
+  const logger = (msg) => {
+    if (mainWindow && !mainWindow.isDestroyed()) {
+      mainWindow.webContents.send('scraper:log', msg);
+    }
+  };
+
+  runGmaps(config, activeTask, logger)
+    .catch((err) => {
+      logger(`Error: ${err.message}`);
+    })
+    .finally(() => {
+      if (activeTask) activeTask.isRunning = false;
+      if (mainWindow && !mainWindow.isDestroyed()) {
+        mainWindow.webContents.send('scraper:done');
+      }
+    });
+
+  return { success: true };
+});
+
+ipcMain.handle('scraper:start-twogis', async (event, config) => {
+  if (activeTask && activeTask.isRunning) {
+    return { success: false, message: 'Another task is currently running.' };
+  }
+
+  activeTask = { isRunning: true, cancelled: false };
+
+  const logger = (msg) => {
+    if (mainWindow && !mainWindow.isDestroyed()) {
+      mainWindow.webContents.send('scraper:log', msg);
+    }
+  };
+
+  runTwoGis(config, activeTask, logger)
+    .catch((err) => {
+      logger(`Error: ${err.message}`);
+    })
+    .finally(() => {
+      if (activeTask) activeTask.isRunning = false;
+      if (mainWindow && !mainWindow.isDestroyed()) {
+        mainWindow.webContents.send('scraper:done');
+      }
+    });
+
+  return { success: true };
+});
+
+ipcMain.handle('scraper:stop', async () => {
+  if (activeTask) {
+    activeTask.cancelled = true;
+  }
+  return true;
+});
